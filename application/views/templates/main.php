dd0eb87bc25e4828c345cee68117031b90156386
Robert Doucette <rice8204@gmail.com>
2011-09-13 00:07:26 +0200

    adding in the jQuery UI javascript file

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -58,6 +58,7 @@
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.3.min.js"><\/script>')</script>
+  <script src="js/libs/jquery-ui-1.8.16.js"></script>
 
 
   <!-- scripts concatenated and minified via build script -->
